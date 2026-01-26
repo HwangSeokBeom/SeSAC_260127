8b730fb090c84728b5804b4b2b42d7aefebd8ee9
HwangSeokBeom <tjrqja0714@gmail.com>
2026-01-26 15:46:17 +0900

    Initial Commit

---
+++ SeSAC_260127/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_260127
+//
+//  Created by Hwangseokbeom on 1/26/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
