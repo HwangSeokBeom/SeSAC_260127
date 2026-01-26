6e0a31563ee53dba3f600f10f093c866b444be60
HwangSeokBeom <tjrqja0714@gmail.com>
2026-01-26 16:12:00 +0900

    [Feat]: 레파지토리 구현

--- SeSAC_260127/Repository/DefaultTopicRepository.swift
+++ SeSAC_260127/Repository/DefaultTopicRepository.swift
@@ -1,1 +1,21 @@
+//
+//  DefaultTopicRepository.swift
+//  SeSAC_260127
+//
+//  Created by Hwangseokbeom on 1/26/26.
+//
 
+import Foundation
+
+final class DefaultTopicRepository: TopicRepository {
+
+    private let api: TopicAPIService
+
+    init(api: TopicAPIService) {
+        self.api = api
+    }
+
+    func fetchTopics(completion: @escaping (Result<[TopicSection], Error>) -> Void) {
+        api.requestTopicSections(completion: completion)
+    }
+}
--- SeSAC_260127/Repository/Protocol/TopicRepository.swift
+++ SeSAC_260127/Repository/Protocol/TopicRepository.swift
@@ -1,1 +1,12 @@
+//
+//  TopicRepository.swift
+//  SeSAC_260127
+//
+//  Created by Hwangseokbeom on 1/26/26.
+//
 
+import Foundation
+
+protocol TopicRepository {
+    func fetchTopics(completion: @escaping (Result<[TopicSection], Error>) -> Void)
+}
