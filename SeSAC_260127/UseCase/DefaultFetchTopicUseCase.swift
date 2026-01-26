76ccf07d6d4f049ae1e52d83d66823bb9275974a
HwangSeokBeom <tjrqja0714@gmail.com>
2026-01-26 16:13:25 +0900

    [Feat]: 유즈케이스 구현

--- SeSAC_260127/UseCase/DefaultFetchTopicUseCase.swift
+++ SeSAC_260127/UseCase/DefaultFetchTopicUseCase.swift
@@ -1,1 +1,21 @@
+//
+//  DefaultFetchTopicUseCase.swift
+//  SeSAC_260127
+//
+//  Created by Hwangseokbeom on 1/26/26.
+//
 
+import Foundation
+
+final class FetchTopicUseCase: FetchTopicUseCasing {
+
+    private let repository: TopicRepository
+
+    init(repository: TopicRepository) {
+        self.repository = repository
+    }
+
+    func fetchTopics(completion: @escaping (Result<[TopicSection], Error>) -> Void) {
+        repository.fetchTopics(completion: completion)
+    }
+}
--- SeSAC_260127/UseCase/Protocol/FetchTopicUseCase.swift
+++ SeSAC_260127/UseCase/Protocol/FetchTopicUseCase.swift
@@ -1,1 +1,12 @@
+//
+//  FetchTopicUseCase.swift
+//  SeSAC_260127
+//
+//  Created by Hwangseokbeom on 1/26/26.
+//
 
+import Foundation
+
+protocol FetchTopicUseCasing {
+    func fetchTopics(completion: @escaping (Result<[TopicSection], Error>) -> Void)
+}
