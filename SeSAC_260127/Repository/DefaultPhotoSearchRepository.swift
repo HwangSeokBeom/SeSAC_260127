f68d0a1491d63d36aa1883b6484b1f584b05dc11
HwangSeokBeom <tjrqja0714@gmail.com>
2026-01-27 16:41:20 +0900

    [Feat]: 검색화면 레파지토리 객체 구현

---
+++ SeSAC_260127/Repository/DefaultPhotoSearchRepository.swift
@@ -0,0 +1,26 @@
+//
+//  DefaultPhotoSearchRepository.swift
+//  SeSAC_260127
+//
+//  Created by Hwangseokbeom on 1/27/26.
+//
+
+import Foundation
+
+final class DefaultPhotoSearchRepository: PhotoSearchRepository {
+    
+    private let service: PhotoSearchServicing
+    
+    init(service: PhotoSearchServicing) {
+        self.service = service
+    }
+    
+    func searchPhotos(
+        query: String,
+        color: PhotoColorFilter?,
+        sort: PhotoSortOption,
+        completion: @escaping (Result<[Photo], Error>) -> Void
+    ) {
+        service.search(query: query, color: color, sort: sort, completion: completion)
+    }
+}
---
+++ SeSAC_260127/Repository/Protocol/PhotoSearchRepository.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoSearchRepository.swift
+//  SeSAC_260127
+//
+//  Created by Hwangseokbeom on 1/27/26.
+//
+
+protocol PhotoSearchRepository {
+    func searchPhotos(
+        query: String,
+        color: PhotoColorFilter?,
+        sort: PhotoSortOption,
+        completion: @escaping (Result<[Photo], Error>) -> Void
+    )
+}
