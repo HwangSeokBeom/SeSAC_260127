5fbc0f154aae238ef01ac735051e81b9d9459ec0
HwangSeokBeom <tjrqja0714@gmail.com>
2026-01-27 16:37:14 +0900

    [Feat]: 서치화면 모델 구현

---
+++ SeSAC_260127/Model/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  SeSAC_260127
+//
+//  Created by Hwangseokbeom on 1/27/26.
+//
+
+import Foundation
+
+struct Photo {
+    let id: String
+    let imageURL: URL?
+    let likeCount: Int
+    let isFavorite: Bool
+}
--- SeSAC_260127/Model/PhotoItem.swift
+++ SeSAC_260127/Model/PhotoItem.swift
@@ -5,8 +5,18 @@
 //  Created by Hwangseokbeom on 1/27/26.
 //
 
+import Foundation
+
 struct PhotoItem {
-    let imageName: String    
-    let likeCount: Int
-    var isFavorite: Bool
+    let imageURL: URL?
+    let likeCountText: String
+    let isFavorite: Bool
+}
+
+extension PhotoItem {
+    init(photo: Photo) {
+        self.imageURL = photo.imageURL
+        self.likeCountText = "\(photo.likeCount)"
+        self.isFavorite = photo.isFavorite
+    }
 }
